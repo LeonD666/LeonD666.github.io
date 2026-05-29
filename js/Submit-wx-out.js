df3c02cb2ba7a1a0bf0351ad23843072257efa39
LeonD666 <duanzhongliang666@163.com>
2026-05-29 20:20:51 +0800

    9th update

--- js/Submit-wx-out.js
+++ js/Submit-wx-out.js
@@ -2,7 +2,7 @@ console.log('Supabase loaded:', typeof supabase); // 调试用，上线后可删
 
 const supabaseClient = supabase.createClient({
   url: 'https://vslbcwnuqlgegvfnrfej.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzbGJjd251cWxnZWd2Zm5yZmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMzY0NjIsImV4cCI6MjA5NTYxMjQ2Mn0.tSKIrkx4VoAzxllVq34Xme8ToAP1yHLDI_rCMDWykEQ'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzbGJjd251cWxnZWd2Zm5yZmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMzY0NjIsImV4cCI6MjA5NTYxMjQ2Mn0.tSKIrkx4VoAzxllVq34Xme8ToAP1yHLDI_rCMDWykEQ'
 });
 
 console.log('Supabase client created:', supabaseClient); // 调试用
