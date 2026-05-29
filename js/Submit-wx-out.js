5378c2315bfd52f35039e1eab1d2aa64af72cb03
LeonD666 <duanzhongliang666@163.com>
2026-05-29 20:33:52 +0800

    10th update

--- js/Submit-wx-out.js
+++ js/Submit-wx-out.js
@@ -1,7 +1,7 @@
 console.log('Supabase loaded:', typeof supabase); // 调试用，上线后可删除
 
 const supabaseClient = supabase.createClient({
-  url: 'https://vslbcwnuqlgegvfnrfej.supabase.co',
+  url: 'https://vslbcwnuqlgegvfnrfej.supabase.co/rest/v1/',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzbGJjd251cWxnZWd2Zm5yZmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMzY0NjIsImV4cCI6MjA5NTYxMjQ2Mn0.tSKIrkx4VoAzxllVq34Xme8ToAP1yHLDI_rCMDWykEQ'
 });
 
